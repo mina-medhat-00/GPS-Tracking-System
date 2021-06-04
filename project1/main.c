624c2f6389d82e5db6cfeb072dfbf055922889fd
YousefGamalFawzi <yousaf_g@rocketmail.com>
2021-06-05 00:42:03 +0200

    add gps distance functions

--- project1/main.c
+++ project1/main.c
@@ -1,6 +1,8 @@
 #include "stdint.h"
 #include "tm4c123gh6pm.h"
-
+#include "math.h"
+#define R 6371000
+#define pi 3.141592654
 void init (){
 	SYSCTL_RCGCGPIO_R |= 0x20;
 	while ((SYSCTL_PRGPIO_R&0x20) == 0)
@@ -14,6 +16,25 @@ void init (){
 	GPIO_PORTF_PCTL_R = ~0x1F;
 	GPIO_PORTF_PUR_R |= 0x11;
 	GPIO_PORTF_DATA_R &= ~0x0E; //initialize output pins to be off
+}
+double distance(long double x)
+{
+    static long double z = 0;
+    z+= x;
+    return z;
+}
+double rad(double w)
+{
+    double v = 0;
+    v = w * pi / 180;
+    return v;
+}
+double gps_distance(double lat1, double lon1, double lat2, double lon2) {
+    double x, y, d;
+    x = rad(lon2 - lon1) * cos(rad(lat1 + lat2) / 2);
+    y = rad(lat1 - lat2);
+    d = sqrt(x * x + y * y) * R;
+    return d;
 }
 
 int main (){
