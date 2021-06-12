5a7480d0168131bee9b6f60f65cd99cd5456b232
YousefGamalFawzi <yousaf_g@rocketmail.com>
2021-06-12 22:58:31 +0200

    LCD display update

--- lcd/main.c
+++ lcd/main.c
@@ -34,6 +34,39 @@ void LCD_data(unsigned char data)
 	GPIO_PORTA_DATA_R=0;
 	sys_delay(5);
 }
-int main(){}
-	
 
+void LCD_init(void)
+ {
+     SYSCTL_RCGCGPIO_R =0X03;//ENABLE PORT A AND PORT B
+     GPIO_PORTA_DIR_R|=0x0E;
+   GPIO_PORTA_DEN_R|=0x0E;
+   GPIO_PORTB_DIR_R=0xFF;
+   sys_delay(50);
+     LCD_command(0x30);
+   sys_delay(20);
+   LCD_command(0x30);
+   sys_delay(10);
+   LCD_command(0x30);
+   LCD_command(0x38); /* set 8-bit data, 2-line, 5x7 font /
+   LCD_command(0x06); / move cursor right /
+   LCD_command(0x01); / clear screen, move cursor to home /
+   LCD_command(0x0F); / turn on display, cursor blinking */
+
+ }
+ 
+ int main(){
+     LCD_init();
+
+     while(1){
+     LCD_command(1); //clear display
+     LCD_command(0x80);//cursor location
+     sys_delay(800);
+      while(data_s!=0x0A){
+         LCD_data(data_s);
+            sys_delay(800);
+     }
+ }
+		 
+}
+ 
+ 
