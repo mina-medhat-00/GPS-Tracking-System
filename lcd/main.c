36884eae1fcb4312be56658433eec2d0a8a9564d
youssefnassar58 <1805742@eng.asu.edu.eg>
2021-06-13 16:22:00 +0200

    LCD_update

--- lcd/main.c
+++ lcd/main.c
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "stdint.h"
 #include "tm4c123gh6pm.h"
- unsigned char data_s=0;
 void sys_delay(uint32_t time)
 {
     //enter time in milli second
@@ -19,10 +18,12 @@ void LCD_command(unsigned char command)
 { 
 	GPIO_PORTA_DATA_R =0;
 	GPIO_PORTB_DATA_R=command;
-  GPIO_PORTA_DATA_R =0x80;//port a bit 7,5 enable
+  GPIO_PORTA_DATA_R =0x80;//port a bit 7 enable
 	sys_delay(5);
 	GPIO_PORTA_DATA_R=0;
 	sys_delay(5);
+	
+	
 }
 
 void LCD_data(unsigned char data)
@@ -37,36 +38,41 @@ void LCD_data(unsigned char data)
 
 void LCD_init(void)
  {
-     SYSCTL_RCGCGPIO_R =0X03;//ENABLE PORT A AND PORT B
-     GPIO_PORTA_DIR_R|=0x0E;
-   GPIO_PORTA_DEN_R|=0x0E;
+   SYSCTL_RCGCGPIO_R =0X03;//ENABLE PORT A AND PORT B
+	 while ((SYSCTL_PRGPIO_R&0x03)==0){};
+   GPIO_PORTA_DIR_R|=0xE0;
+   GPIO_PORTA_DEN_R|=0xE0;
    GPIO_PORTB_DIR_R=0xFF;
+	 GPIO_PORTB_DEN_R|=0xFF; 
    sys_delay(50);
-     LCD_command(0x30);
+   LCD_command(0x30);
    sys_delay(20);
    LCD_command(0x30);
-   sys_delay(10);
-   LCD_command(0x30);
-   LCD_command(0x38); // set 8-bit data, 2-line, 5x7 font 
-   LCD_command(0x06); // move cursor right 
-   LCD_command(0x01); //clear screen, move cursor to home 
-   LCD_command(0x0F); // turn on display, cursor blinking 
-
- }
+   sys_delay(20);
+   LCD_command(0x30); // screen wake up
+	 sys_delay(20);
+   LCD_command(0x38); // set 8-bit data, 2-line, 5x7 font
+   sys_delay(10);		 
+   LCD_command(0x06); // move cursor right
+	 sys_delay(10);
+   LCD_command(0x01); // clear screen, move cursor to home 
+   sys_delay(10); 
+   LCD_command(0x0F); // turn on display, cursor blinking
+		sys_delay(10); 
+}
  
  int main(){
+	 unsigned char data_s=0x41;
      LCD_init();
-
      while(1){
      LCD_command(1); //clear display
      LCD_command(0x80);//cursor location
-     sys_delay(800);
-      while(data_s!=0x0A){
-         LCD_data(data_s);
-            sys_delay(800);
+     sys_delay(500);
+		 LCD_data(data_s);
+		 LCD_command(0x06);//move cursor
+			sys_delay(500); 
+			 
+			 
+    
      }
  }
-		 
-}
- 
-
