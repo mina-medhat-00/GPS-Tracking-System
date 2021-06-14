85edeb5c72ec19f569faad9a88cea2bcbe04121a
youssefnassar58 <1805742@eng.asu.edu.eg>
2021-06-15 00:34:26 +0200

    Revert "add distance function"
    This reverts commit 8766f3a816168e547bb34ad1f94a9d1e99b36fbf.

--- 7 segments using 3 displays/main.d
+++ 7 segments using 3 displays/main.d
@@ -1,6 +1,3 @@
 .\main.o: main.c
 .\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
-.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
 .\main.o: tm4c123gh6pm.h
-.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
